c5ffb2f0718c14e3b64a9c585bec0ad9e5ddda62
Azenga Kevin <toomuch573@gmail.com>
2018-12-03 20:31:25 +0300

    Tried to add a testing navbar for the added bootstrap files

--- index.php
+++ index.php
@@ -5,8 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ClassApp</title>
+    <link href="css/bootstrap.min.css" />
 </head>
 <body>
     <!-- I think am going to use codeignitor in this project -->
+
+    <header>
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <a href="index.php" class="navbar-brand">CS2015</a>
+
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link">Home</a></li>
+                <li class="nav-item"><a class="nav-link">About Me</a></li>
+                <li class="nav-item"><a class="nav-link">Contact Me</a></li>
+            </ul>
+        </nav>
+
+    </header>
+
+    <script href="js/bootstrap.min.js"></script>
 </body>
 </html>
