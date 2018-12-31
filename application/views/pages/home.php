de7b1ed329686ac55a469ed0df979a389a7c2f9e
Azenga Kevin <toomuch573@gmail.com>
2018-12-31 10:18:43 +0300

    Changed alert message to update account

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,7 +2,7 @@
 This is the home page <br />
 <?php if($this->session->userdata('logged_in')): ?>
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <a href="index.php/user/edit">Here</a> Finishing setting account
+        <a href="index.php/user/edit">Update account</a> Finishing setting account
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
